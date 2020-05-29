9394f9827c1f910afdcb1b5b9dff570e2ac6e770
wayn <1669738430@qq.com>
2020-05-29 15:20:31 +0800

    feat(后台管理): 添加优惠卷状态检查定时任务

---
+++ src/main/java/ltd/newbee/mall/config/TaskConfig.java
@@ -0,0 +1,43 @@
+package ltd.newbee.mall.config;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import ltd.newbee.mall.entity.CouponUser;
+import ltd.newbee.mall.service.CouponUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+
+@EnableScheduling
+@Configuration
+@EnableAsync
+@Slf4j
+public class TaskConfig {
+
+    @Autowired
+    private CouponUserService couponUserService;
+
+    /**
+     * 每天凌晨2点执行任务，检查用户领取的优惠卷是否过期<br>
+     * cron = "0 0 2 * * ?"
+     */
+    @Async
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void checkCouponStatus() {
+        log.info("检查用户领取的优惠卷是否过期任务:开始");
+        List<CouponUser> list = couponUserService.list(new QueryWrapper<CouponUser>().eq("status", 0));
+        for (CouponUser couponUser : list) {
+            if (LocalDate.now().isAfter(couponUser.getEndTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())) {
+                couponUserService.update().set("status", 2).eq("coupon_user_id", couponUser.getCouponUserId()).update();
+            }
+        }
+        log.info("检查用户领取的优惠卷是否过期任务:结束");
+    }
+}
