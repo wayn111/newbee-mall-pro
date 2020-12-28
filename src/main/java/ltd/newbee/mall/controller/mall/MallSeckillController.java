7e709fad06daa7536cf0ba3b6b5c9857430aefe1
wayn <1669738430@qq.com>
2020-12-28 22:45:17 +0800

    perf(商城): 秒杀专区
    美化样式

--- src/main/java/ltd/newbee/mall/controller/mall/MallSeckillController.java
+++ src/main/java/ltd/newbee/mall/controller/mall/MallSeckillController.java
@@ -2,22 +2,30 @@ package ltd.newbee.mall.controller.mall;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import ltd.newbee.mall.constant.Constants;
 import ltd.newbee.mall.entity.Goods;
 import ltd.newbee.mall.entity.Seckill;
+import ltd.newbee.mall.entity.ShopCat;
+import ltd.newbee.mall.entity.vo.MallUserVO;
 import ltd.newbee.mall.service.GoodsService;
 import ltd.newbee.mall.service.SeckillService;
+import ltd.newbee.mall.service.ShopCatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("seckill")
 public class MallSeckillController {
 
     @Autowired
@@ -26,21 +34,53 @@ public class MallSeckillController {
     @Autowired
     private GoodsService goodsService;
 
-    @GetMapping("seckill")
-    public String index(HttpServletRequest request, HttpSession session) {
+    @Autowired
+    private ShopCatService shopCatService;
+
+    @GetMapping("list")
+    public String list(HttpServletRequest request, HttpSession session) {
         List<Seckill> seckillList = seckillService.list(new QueryWrapper<Seckill>().eq("status", 1).orderByDesc("seckill_rank"));
         List<Map<String, Object>> list = seckillList.stream().map(seckill -> {
             Map<String, Object> map = new HashMap<>();
+            map.put("seckillId", seckill.getSeckillId());
             map.put("goodsId", seckill.getGoodsId());
             map.put("seckillPrice", seckill.getSeckillPrice());
             Goods goods = goodsService.getById(seckill.getGoodsId());
             map.put("goodsName", goods.getGoodsName());
-            map.put("originPrice", goods.getOriginalPrice());
+            map.put("originalPrice", goods.getOriginalPrice());
             map.put("goodsCoverImg", goods.getGoodsCoverImg());
             map.put("goodsIntro", goods.getGoodsIntro());
             return map;
         }).collect(Collectors.toList());
         request.setAttribute("seckillList", list);
-        return "mall/seckill";
+        return "mall/seckill-list";
+    }
+
+
+    @GetMapping("detail/{seckillId}")
+    public String detail(@PathVariable("seckillId") Long seckillId, HttpServletRequest request, HttpSession session) {
+        Seckill seckill = seckillService.getById(seckillId);
+        Long goodsId = seckill.getGoodsId();
+        // 查询购物车中是否有该商品
+        ShopCat shopCat = shopCatService.getOne(new QueryWrapper<ShopCat>()
+                .eq("user_id", ((MallUserVO) session.getAttribute(Constants.MALL_USER_SESSION_KEY)).getUserId())
+                .eq("goods_id", goodsId));
+        request.setAttribute("goodsCount", 0);
+        if (Objects.nonNull(shopCat)) {
+            request.setAttribute("cartItemId", shopCat.getCartItemId());
+            request.setAttribute("goodsCount", shopCat.getGoodsCount());
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("goodsId", goodsId);
+        map.put("seckillPrice", seckill.getSeckillPrice());
+        Goods goods = goodsService.getById(seckill.getGoodsId());
+        map.put("goodsName", goods.getGoodsName());
+        map.put("goodsIntro", goods.getGoodsIntro());
+        map.put("originalPrice", goods.getOriginalPrice());
+        map.put("goodsCoverImg", goods.getGoodsCoverImg());
+        map.put("goodsIntro", goods.getGoodsIntro());
+        map.put("goodsDetailContent", goods.getGoodsDetailContent());
+        request.setAttribute("goodsDetail", map);
+        return "mall/seckill-detail";
     }
 }
