28dfdc39f2720210781da94ca8e4c3f4c32269db
hequan_waynaqua <1669738430@qq.com>
2021-02-04 21:38:28 +0800

    feat(商城): 购物车
    优化

--- src/main/java/ltd/newbee/mall/intercepter/MallShopCartNumberInterceptor.java
+++ src/main/java/ltd/newbee/mall/intercepter/MallShopCartNumberInterceptor.java
@@ -19,6 +19,7 @@ public class MallShopCartNumberInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // todo 删除这个逻辑
         // 购物车中的数量会更改，但是在这些接口中并没有对session中的数据做修改，这里统一处理一下
         if (null != request.getSession() && null != request.getSession().getAttribute(Constants.MALL_USER_SESSION_KEY)) {
             // 如果当前为登陆状态，就查询数据库并设置购物车中的数量值
