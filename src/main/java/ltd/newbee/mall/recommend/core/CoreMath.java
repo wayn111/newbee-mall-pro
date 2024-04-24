84bf182ae34186eeb16139256bba3283a1dbc722
waynaqua <1669738430@qq.com>
2024-04-24 11:41:01 +0800

    feat(商城): 完善注释

--- src/main/java/ltd/newbee/mall/recommend/core/CoreMath.java
+++ src/main/java/ltd/newbee/mall/recommend/core/CoreMath.java
@@ -1,10 +1,12 @@
-package ltd.newbee.mall.recommend.core;
+package ltd.newbee.mall.recommend.core; // 包声明
 
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import ltd.newbee.mall.recommend.dto.RelateDTO;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -13,99 +15,98 @@ public class CoreMath {
     /**
      * 计算相关系数并排序
      *
-     * @param key
-     * @param map
-     * @param type 类型0基于用户推荐使用余弦相似度 1基于物品推荐使用余弦相似度 2基于用户推荐使用皮尔森系数计算
-     * @return Map<Double, Long>
+     * @param key  代表当前用户或物品的唯一标识
+     * @param map  存储用户或物品与对应关系数据的映射
+     * @param type 确定使用哪种方式计算相关系数：0代表基于用户的余弦相似度，1代表基于物品的余弦相似度，2代表使用皮尔森系数
+     * @return 返回一个根据相关系数排序的映射，映射的键为相关系数，值为用户或物品的唯一标识
      */
     public static Map<Double, Long> computeNeighbor(Long key, Map<Long, List<RelateDTO>> map, int type) {
+        // 使用TreeMap存储相关系数和对应键值对，确保结果有序
         Map<Double, Long> distMap = new TreeMap<>();
+        // 获取当前用户或物品的关系数据
         List<RelateDTO> items = map.get(key);
         map.forEach((k, v) -> {
-            // 排除此用户
+            // 排除当前用户或物品
             if (!k.equals(key)) {
-                // 关系系数
+                // 计算相关系数
                 double coefficient = relateDist(v, items, type);
-                // 关系距离
-                // double distance=Math.abs(coefficient);
+                // 将系数和标识加入映射中
                 distMap.put(coefficient, k);
             }
         });
+        // 返回结果
         return distMap;
     }
 
     /**
      * 计算两个序列间的相关系数
      *
-     * @param xList
-     * @param yList
-     * @param type  类型0基于用户推荐使用余弦相似度 1基于物品推荐使用余弦相似度 2基于用户推荐使用皮尔森系数计算
-     * @return
+     * @param xList 第一个用户或物品的关系数据列表
+     * @param yList 第二个用户或物品的关系数据列表
+     * @param type  计算方式标识
+     * @return 返回两个序列间的相关系数
      */
     private static double relateDist(List<RelateDTO> xList, List<RelateDTO> yList, Integer type) {
+        // 存储第一个列表的相关数据
         List<Integer> xs = Lists.newArrayList();
+        // 存储第二个列表的相关数据
         List<Integer> ys = Lists.newArrayList();
         xList.forEach(x -> yList.forEach(y -> {
-            if (type == 0 || type == 2) {
-                // 基于用户推荐时如果两个用户购买的商品相同，则计算相似度
-                if (x.getProductId().longValue() == y.getProductId().longValue()) {
-                    xs.add(x.getIndex());
-                    ys.add(y.getIndex());
-                }
-            } else if (type == 1) {
-                // 基于物品推荐时如果两个用户id相同，则计算相似度
-                if (x.getUserId().longValue() == y.getUserId().longValue()) {
-                    xs.add(x.getIndex());
-                    ys.add(y.getIndex());
-                }
+            if ((type == 0 || type == 2) && x.getProductId().longValue() == y.getProductId().longValue()) {
+                xs.add(x.getIndex());
+                ys.add(y.getIndex());
+            } else if (type == 1 && x.getUserId().longValue() == y.getUserId().longValue()) {
+                xs.add(x.getIndex());
+                ys.add(y.getIndex());
             }
-
         }));
-        if (ys.size() == 0 || xs.size() == 0) {
+        if (ys.isEmpty() || xs.isEmpty()) {
+            // 如果一个序列为空，则相关系数为0
             return 0d;
         }
         if (type == 2) {
+            // 使用皮尔森系数计算
             return pearsonRelate(xs, ys);
         } else {
+            // 使用余弦相似度计算
             return cosineSimilarity(xs, ys);
         }
     }
 
     /**
-     * 方法描述: 皮尔森（pearson）相关系数计算
-     * 余弦相似度：越接近于 1 ，说明两个用户的浏览行为越相似
+     * 皮尔森（pearson）相关系数计算
      *
-     * @param xs
-     * @param ys
-     * @throws
-     * @Return {@link Double}
-     * @author tarzan
-     * @date 2020年07月31日 17:03:20
+     * @param xs 第一个列表的数据
+     * @param ys 第二个列表的数据
+     * @return 返回两个列表的皮尔森相关系数
      */
     public static double pearsonRelate(List<Integer> xs, List<Integer> ys) {
+        // 各种相关数据计算
         int n = xs.size();
         double Ex = xs.stream().mapToDouble(x -> x).sum();
         double Ey = ys.stream().mapToDouble(y -> y).sum();
         double Ex2 = xs.stream().mapToDouble(x -> Math.pow(x, 2)).sum();
         double Ey2 = ys.stream().mapToDouble(y -> Math.pow(y, 2)).sum();
         double Exy = IntStream.range(0, n).mapToDouble(i -> xs.get(i) * ys.get(i)).sum();
+        // 计算相关系数
         double numerator = Exy - Ex * Ey / n;
         double denominator = Math.sqrt((-Math.pow(Ex, 2) / n) * (Ey2 - Math.pow(Ey, 2) / n));
         if (denominator == 0 || Double.isNaN(numerator) || Double.isNaN(denominator)) {
+            // 如果分母为0，则相关系数为0
             return 0.0;
         }
         return numerator / denominator;
     }
 
-
     /**
      * 计算向量之间的余弦相似度
      *
-     * @param xs
-     * @param xs
-     * @return double
+     * @param xs 第一个向量
+     * @param ys 第二个向量
+     * @return 返回两向量的余弦相似度
      */
     private static double cosineSimilarity(List<Integer> xs, List<Integer> ys) {
+        // 向量点积以及各自向量的范数计算
         double dotProduct = 0;
         double norm1 = 0;
         double norm2 = 0;
@@ -116,6 +117,7 @@ public class CoreMath {
             norm1 += Math.pow(x, 2);
             norm2 += Math.pow(y, 2);
         }
+        // 计算并返回余弦相似度
         return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
     }
 
--- src/main/java/ltd/newbee/mall/recommend/core/UserCF.java
+++ src/main/java/ltd/newbee/mall/recommend/core/UserCF.java
@@ -1,56 +1,60 @@
-package ltd.newbee.mall.recommend.core;
+package ltd.newbee.mall.recommend.core; // 包路径声明
 
 import ltd.newbee.mall.recommend.dto.RelateDTO;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserCF {
     /**
-     * 物用户协同推荐
+     * 实现用户协同过滤推荐算法
      *
      * @param userId 用户ID
-     * @param num    返回数量
-     * @param list   预处理数据
-     * @param type   类型0基于用户推荐使用余弦相似度 1基于物品推荐使用余弦相似度 2基于用户推荐使用皮尔森系数计算
-     * @return 商品id集合
+     * @param num    推荐商品的数量
+     * @param list   预处理的关联数据列表
+     * @param type   计算相似度的类型：0-基于用户的余弦相似度，1-基于物品的余弦相似度，2-基于用户的皮尔森系数
+     * @return 商品ID的列表
      */
     public static List<Long> recommend(Long userId, Integer num,
                                        List<RelateDTO> list, Integer type) {
-        // 对每个用户的购买商品记录进行分组
+        // 用户购买商品数据的分组映射
         Map<Long, List<RelateDTO>> userMap = list.stream()
                 .collect(Collectors.groupingBy(RelateDTO::getUserId));
-        // 获取其他用户与当前用户的关系值
+        // 计算当前用户和其他用户之间的相似度
         Map<Double, Long> userDisMap = CoreMath.computeNeighbor(userId, userMap, type);
+        // 与当前用户相似的用户列表
         List<Long> similarUserIdList = new ArrayList<>();
+        // 相似度值列表
         List<Double> values = new ArrayList<>(userDisMap.keySet());
-        values.sort(Collections.reverseOrder());
+        values.sort(Collections.reverseOrder()); // 将相似度值列表降序排序
+        // 取出相似度最高的三个值
         List<Double> scoresList = values.stream().limit(3).toList();
-        // 获取关系最近的用户
+        // 从相似用户中提取用户ID
         for (Double aDouble : scoresList) {
             similarUserIdList.add(userDisMap.get(aDouble));
         }
+        // 与当前用户相似的产品列表
         List<Long> similarProductIdList = new ArrayList<>();
         for (Long similarUserId : similarUserIdList) {
-            // 获取相似用户购买商品的记录
+            // 获取每个相似用户购买过的产品ID集
             List<Long> collect = userMap.get(similarUserId).stream()
                     .map(RelateDTO::getProductId).toList();
-            // 过滤掉重复的商品
-            List<Long> collect1 = collect.stream()
+            // 过滤掉已经存在于推荐列表中的产品ID，避免重复
+            List<Long> uniqueProducts = collect.stream()
                     .filter(e -> !similarProductIdList.contains(e)).toList();
-            similarProductIdList.addAll(collect1);
+            similarProductIdList.addAll(uniqueProducts);
         }
-        // 当前登录用户购买过的商品
+        // 当前用户已购买的商品ID
         List<Long> userProductIdList = userMap.getOrDefault(userId,
                 Collections.emptyList()).stream().map(RelateDTO::getProductId).toList();
-        // 相似用户买过，但是当前用户没买过的商品作为推荐
-        List<Long> recommendList = new ArrayList<>();
-        for (Long similarProduct : similarProductIdList) {
-            if (!userProductIdList.contains(similarProduct)) {
-                recommendList.add(similarProduct);
-            }
-        }
-        Collections.sort(recommendList);
+        // 推荐那些相似用户购买，但是自己尚未购买的商品
+        List<Long> recommendList = similarProductIdList.stream()
+                .filter(prodId -> !userProductIdList.contains(prodId))
+                .sorted().toList(); // 将推荐列表排序并去重
+        // 返回指定数量的推荐商品ID列表
         return recommendList.stream().distinct().limit(num).toList();
     }
 }
